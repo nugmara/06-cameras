d6f5c098fcaa3e3d982023979bf719eb6cc77366
nugmara <dagmarag0@gmail.com>
2024-04-14 22:19:48 +0200

    Add the called drag and drop with OrbitControls

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 /**
  * Cursor
 */ 
@@ -54,6 +54,9 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -71,11 +74,11 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     // Update camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 5
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
 
-    camera.lookAt(mesh.position)
+    // camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
